b1f3f321badea0a03992106b1626b53c4e663d2b
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-20 18:00:28 +0300

    Interface for StringSplitter

---
+++ src/ru/petrov/lab1/b/StringSplitter.java
@@ -0,0 +1,13 @@
+package ru.petrov.lab1.b;
+
+import java.util.List;
+
+public class StringSplitter {
+    public static List<String> splitAndReplace(String s, int groupLength, int replaceIndex) {
+        return null;
+    }
+
+    public static List<String> labTask(String s) {
+        return null;
+    }
+}
