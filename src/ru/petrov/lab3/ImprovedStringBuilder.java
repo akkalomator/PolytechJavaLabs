552de2c312c22d5c9d58de77716ffd5930b337b6
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-22 15:58:44 +0300

    Creating interface for ImprovedStringBuilder

---
+++ src/ru/petrov/lab3/ImprovedStringBuilder.java
@@ -0,0 +1,190 @@
+package ru.petrov.lab3;
+
+public class ImprovedStringBuilder implements Comparable<ImprovedStringBuilder>, CharSequence {
+
+    public ImprovedStringBuilder() {
+    }
+
+    public ImprovedStringBuilder(int capacity) {
+    }
+
+    public ImprovedStringBuilder(String str) {
+    }
+
+    public ImprovedStringBuilder(CharSequence seq) {
+    }
+
+    public ImprovedStringBuilder append(Object obj) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(String str) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(StringBuffer sb) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(CharSequence s) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(CharSequence s, int start, int end) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(char[] str) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(char[] str, int offset, int len) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(boolean b) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(char c) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(int i) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(long lng) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(float f) {
+        return null;
+    }
+
+    public ImprovedStringBuilder append(double d) {
+        return null;
+    }
+
+    public ImprovedStringBuilder appendCodePoint(int codePoint) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int index, char[] str, int offset, int len) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, Object obj) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, String str) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, char[] str) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int dstOffset, CharSequence s) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int dstOffset, CharSequence s, int start, int end) {
+        return null;
+    }
+
+
+    public ImprovedStringBuilder insert(int offset, boolean b) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, char c) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, int i) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, long l) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, float f) {
+        return null;
+    }
+
+    public ImprovedStringBuilder insert(int offset, double d) {
+        return null;
+    }
+
+    public ImprovedStringBuilder delete(int start, int end) {
+        return null;
+    }
+
+    public ImprovedStringBuilder deleteCharAt(int index) {
+        return null;
+    }
+
+    public ImprovedStringBuilder replace(int start, int end, String str) {
+        return null;
+    }
+
+    public int indexOf(String str) {
+        return 0;
+    }
+
+    public int indexOf(String str, int fromIndex) {
+        return 0;
+    }
+
+    public int lastIndexOf(String str) {
+        return 0;
+    }
+
+
+    public int lastIndexOf(String str, int fromIndex) {
+        return 0;
+    }
+
+
+    public ImprovedStringBuilder reverse() {
+        return null;
+    }
+
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+
+    public String toString() {
+        return null;
+    }
+
+    private void writeObject(java.io.ObjectOutputStream s) {
+    }
+
+
+    private void readObject(java.io.ObjectInputStream s) {
+    }
+
+    @Override
+    public int compareTo(ImprovedStringBuilder o) {
+        return 0;
+    }
+
+    public void undo() {
+    }
+}
