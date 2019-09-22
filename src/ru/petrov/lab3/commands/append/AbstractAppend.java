f213e7f3aab1e65d9c918421d7c1bae1d7c5259c
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-22 23:44:01 +0300

    Refactoring in AbstractAppend

--- src/ru/petrov/lab3/commands/append/AbstractAppend.java
+++ src/ru/petrov/lab3/commands/append/AbstractAppend.java
@@ -8,17 +8,16 @@ public abstract class AbstractAppend extends ImprovedStringBuilderCommand {
 
     protected AbstractAppend(StringBuilder builder, int wordLength) {
         super(builder);
+        if (wordLength < 0) {
+            throw new IllegalArgumentException("Word length must be greater than 0 or equal to it");
+        }
         this.wordLength = wordLength;
     }
 
     @Override
     public void unExecute() {
-
         super.unExecute();
-
         builder.delete(builder.length() - wordLength, builder.length());
-
         super.afterUnexecuted();
-
     }
 }
---
+++ test/ru/petrov/lab3/commands/append/AbstractAppendTest.java
@@ -0,0 +1,15 @@
+package ru.petrov.lab3.commands.append;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractAppendTest {
+
+    @Test
+    public void constructor_ThrowsOnWordLengthLessThanZero() {
+        StringBuilder builder = new StringBuilder();
+        assertThrows(IllegalArgumentException.class, () -> new AbstractAppend(builder, -1) {});
+    }
+
+}
