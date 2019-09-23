e8b9605ac60f0a0859a557f66e28eb5f52c474b3
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-23 17:30:57 +0300

    Adding main

---
+++ src/ru/petrov/lab4/Main.java
@@ -0,0 +1,9 @@
+package ru.petrov.lab4;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ConsoleController.run();
+    }
+}
