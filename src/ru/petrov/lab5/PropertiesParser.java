6c1f7b26763959cee57804e4aec506b5964ca046
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-23 19:56:29 +0300

    Creating interface for PropertiesParser

---
+++ src/ru/petrov/lab5/PropertiesParser.java
@@ -0,0 +1,21 @@
+package ru.petrov.lab5;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class PropertiesParser {
+    private final InputStream inputStream;
+
+    public PropertiesParser(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    public void read() {
+
+    }
+
+    public Map<String, String> getProperties() {
+        return null;
+    }
+
+}
