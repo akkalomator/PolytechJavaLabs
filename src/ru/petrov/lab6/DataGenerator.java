03e63ebd7d135fdb5ef3468f942e8cd58c239cc8
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-24 16:15:34 +0300

    Generating transaction data

---
+++ src/ru/petrov/lab6/DataGenerator.java
@@ -0,0 +1,33 @@
+package ru.petrov.lab6;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class DataGenerator {
+
+    private static final int TRANSACTION_NUMBER = 10000;
+    private static final int USERS_NUMBER = 100;
+    private static final int MAX_SUM = 50000;
+
+    public static void main(String[] args) throws IOException {
+        Random r = new Random();
+        FileWriter writer = new FileWriter("resources/lab6/data.xml");
+        writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<transactions>\n"
+        );
+
+        for (int i = 0; i < TRANSACTION_NUMBER; i++) {
+            writer.write(String.format(
+                "\t<transaction id=\"%d\" from=\"%d\" to=\"%d\" amount=\"%d\" />\n",
+                i,
+                r.nextInt(USERS_NUMBER),
+                r.nextInt(USERS_NUMBER),
+                r.nextInt(MAX_SUM)
+            ));
+        }
+
+        writer.write("</transactions>");
+        writer.close();
+    }
+}
