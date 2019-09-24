d2c8ecce1de0f8104a476806299fa538d1041d0d
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-24 16:57:47 +0300

    Implementing Transaction

--- src/ru/petrov/lab6/Transaction.java
+++ src/ru/petrov/lab6/Transaction.java
@@ -2,11 +2,35 @@ package ru.petrov.lab6;
 
 public class Transaction {
 
+    private final int id;
+    private final Account from;
+    private final Account to;
+    private final int amount;
+
     public Transaction(int id, Account from, Account to, int amount) {
 
+        if (from == null || to == null) {
+            throw new IllegalArgumentException("Accounts cannot be be null");
+        }
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount cannot be negative, try to swap sender and recipient of transaction");
+        }
+
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
     }
 
     public void completeTransaction() {
+        if (!from.haveEnoughMoney(amount)) {
+            throw new TransactionFailedException("Not enough money");
+        }
+        from.changeAmount(-this.amount);
+        to.changeAmount(this.amount);
+    }
 
+    public int getId() {
+        return id;
     }
 }
