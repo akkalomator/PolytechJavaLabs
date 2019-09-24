d2925c20ae68c451ea2cdf9519d8b867bf7087dc
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-24 13:03:44 +0300

    Rewriting PropertiesParser and adding more tests

--- src/ru/petrov/lab5/PropertiesParser.java
+++ src/ru/petrov/lab5/PropertiesParser.java
@@ -4,83 +4,163 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PropertiesParser {
     private final InputStream inputStream;
     private Map<String, String> properties;
+    private boolean precedesWithBackSlash;
+    private String storedKey;
+    private StringBuilder valueBuilder;
 
     public PropertiesParser(InputStream inputStream) {
         this.inputStream = inputStream;
     }
 
-    public void parse() throws IOException {
+    public void parse() throws ParseException, IOException {
         try (
             InputStreamReader r = new InputStreamReader(inputStream);
             BufferedReader reader = new BufferedReader(r)
         ) {
             properties = new HashMap<>();
-
-            final StringBuilder[] multiLineValue = {new StringBuilder()};
-            final boolean[] isPreviousLineFinished = {true};
-            final String[] key = {null};
-
-            reader.lines().forEach(
-
-                rawLine -> {
-                    String line = rawLine.trim();
-                    if (!(line.startsWith("!") || line.startsWith("#") || line.isBlank())) {
-                        if (isPreviousLineFinished[0]) {
-                            String[] tk = line.split("[=:]",2);
-                            key[0] = tk[0].trim().replace("\\ ", " ");
-                            tk[1] = tk[1].trim();
-
-                            if (tk[1].endsWith("\\")) {
-                                isPreviousLineFinished[0] = false;
-                                multiLineValue[0]
-                                    .append(tk[1].replace("\\\\", "\\"))
-                                    .deleteCharAt(multiLineValue[0].length() - 1);
-                            } else {
-                                isPreviousLineFinished[0] = true;
-                                if (tk[1].charAt(0) == '\\' && tk[1].charAt(1) == 'u') {
-
-                                    String codeString = tk[1].substring(2);
-                                    short code = Short.valueOf(codeString);
-                                    char c = (char) code;
-
-                                    properties.put(key[0], String.valueOf(c));
-
-                                } else {
-                                    properties.put(key[0], multiLineValue[0].toString() + tk[1].replace("\\\\", "\\"));
-                                }
-
-                                if (multiLineValue[0].length() != 0) {
-                                    multiLineValue[0] = new StringBuilder();
-                                }
-                            }
-                        } else {
-                            if (line.endsWith("\\")) {
-                                isPreviousLineFinished[0] = false;
-                                multiLineValue[0]
-                                    .append(line.replace("\\\\", "\\"))
-                                    .deleteCharAt(multiLineValue[0].length() - 1);
-                            } else {
-                                isPreviousLineFinished[0] = true;
-                                properties.put(key[0], multiLineValue[0].toString() + line.replace("\\\\", "\\"));
-                                if (multiLineValue[0].length() != 0) {
-                                    multiLineValue[0] = new StringBuilder();
-                                }
-                            }
-                        }
+            valueBuilder = new StringBuilder();
+            try {
+                reader.lines().forEach(rawLine -> {
+                    try {
+                        parseLine(rawLine);
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
                     }
+                });
+            } catch (RuntimeException e) {
+                if (e.getCause() instanceof ParseException) {
+                    throw (ParseException) e.getCause();
+                } else {
+                    throw e;
                 }
-            );
+            }
         }
     }
 
+    private void parseLine(String rawLine) throws ParseException {
+        String line = rawLine.trim();
+
+        if (line.startsWith("!") || line.startsWith("#") || line.isBlank()) {
+            return;
+        }
+
+        if (precedesWithBackSlash) {
+            precedesWithBackSlash = false;
+            valueBuilder.append(parseValue(line, 0));
+            if (precedesWithBackSlash) {
+                return;
+            }
+            properties.put(storedKey, valueBuilder.toString());
+            return;
+        }
+
+        if (valueBuilder.length() != 0) {
+            valueBuilder = new StringBuilder();
+        }
+
+        int lastIndex;
+        for (lastIndex = 0; lastIndex < line.length(); lastIndex++) {
+            char c = line.charAt(lastIndex);
+            if (c == ':' || c == '=') {
+                break;
+            }
+            if (c == '\\') {
+                precedesWithBackSlash = true;
+                continue;
+            }
+            if (c == ' ' && precedesWithBackSlash) {
+                precedesWithBackSlash = false;
+                continue;
+            }
+        }
+
+        if (lastIndex == line.length()) {
+            throw new ParseException("No value found at line " + line, 0);
+        }
+
+        String key = parseKey(line, lastIndex);
+        String value = parseValue(line, lastIndex + 1);
+
+        if (precedesWithBackSlash) {
+            storedKey = key;
+            valueBuilder.append(value).deleteCharAt(valueBuilder.length() - 1);
+        }
+        properties.put(key, value);
+    }
+
+    private String parseKey(String line, int keyLastIndex) throws ParseException {
+        String key = line.substring(0, keyLastIndex).trim();
+        if (!key.contains(" ")) {
+            return key;
+        }
+        StringBuilder keyBuilder = new StringBuilder(key);
+        for (int i = 0; i < keyBuilder.length(); i++) {
+            if (keyBuilder.charAt(i) == ' ') {
+                if (keyBuilder.charAt(i - 1) != '\\') {
+                    throw new ParseException("Invalid key at line " + line, i);
+                }
+                keyBuilder.deleteCharAt(i - 1);
+            }
+        }
+        return keyBuilder.toString();
+
+    }
+
+    private String parseValue(String line, int valueFirstIndex) throws ParseException {
+        StringBuilder builder = new StringBuilder(line.substring(valueFirstIndex).trim());
+        precedesWithBackSlash = false;
+        for (int i = 0; i < builder.length(); i++) {
+            if (builder.charAt(i) == '\\') {
+                if (precedesWithBackSlash) {
+                    builder.deleteCharAt(i);
+                    precedesWithBackSlash = false;
+                    continue;
+                }
+                precedesWithBackSlash = true;
+                continue;
+            }
+            if (builder.charAt(i) == 'u' && precedesWithBackSlash) {
+                readCharValue(builder, i + 1);
+                precedesWithBackSlash = false;
+                continue;
+            }
+        }
+        return builder.toString();
+    }
+
+    private void readCharValue(StringBuilder builder, int i) throws ParseException {
+        String symbol = builder.substring(i, i + 4);
+        short sh;
+        try {
+            sh = Short.valueOf(symbol);
+        } catch (NumberFormatException e) {
+            throw new ParseException("Invalid ASCII symbol: " + symbol, i);
+        }
+        char c = (char) sh;
+        builder.replace(i - 2, i + 4, String.valueOf(c));
+    }
+
     public Map<String, String> getProperties() {
+        if (properties == null) {
+            throw new IllegalStateException("Properties has not been read yet");
+        }
         return properties;
     }
 
+    public String get(String key) {
+        if (properties == null) {
+            throw new IllegalStateException("Properties has not been read yet");
+        }
+        if (!properties.containsKey(key)) {
+            throw new IllegalArgumentException("No such key: " + key);
+        }
+        return properties.get(key);
+    }
 }
--- test/ru/petrov/lab5/PropertiesParserTest.java
+++ test/ru/petrov/lab5/PropertiesParserTest.java
@@ -3,15 +3,29 @@ package ru.petrov.lab5;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PropertiesParserTest {
 
     @Test
-    public void parse_WorksCorrectly() throws IOException {
+    public void parse_ThrowsOnMalformedInput() throws FileNotFoundException {
+        PropertiesParser parser = new PropertiesParser(new FileInputStream("resources/lab5/malformed1.properties"));
+        assertThrows(ParseException.class, parser::parse);
+
+        parser = new PropertiesParser(new FileInputStream("resources/lab5/malformed2.properties"));
+        assertThrows(ParseException.class, parser::parse);
+
+        parser = new PropertiesParser(new FileInputStream("resources/lab5/malformed3.properties"));
+        assertThrows(ParseException.class, parser::parse);
+    }
+
+    @Test
+    public void parse_WorksCorrectly() throws IOException, ParseException {
         PropertiesParser parser = new PropertiesParser(new FileInputStream("resources/lab5/prop.properties"));
         parser.parse();
         Map<String, String> resultProperties = parser.getProperties();
@@ -23,7 +37,35 @@ class PropertiesParserTest {
             "tab", "\u0009",
             "path", "c:\\wiki\\templates"
         );
+
         assertEquals(expectedProperties, resultProperties);
     }
 
+    @Test
+    public void getProperties_ThrowsOnPropertiesHasNotBeenRead() throws FileNotFoundException {
+        PropertiesParser parser = new PropertiesParser(new FileInputStream("resources/lab5/prop.properties"));
+        assertThrows(IllegalStateException.class, parser::getProperties);
+    }
+
+    @Test
+    public void get_ThrowsOnPropertiesHasNotBeenRead() throws FileNotFoundException {
+        PropertiesParser parser = new PropertiesParser(new FileInputStream("resources/lab5/prop.properties"));
+        assertThrows(IllegalStateException.class, () -> parser.get("tab"));
+    }
+
+    @Test
+    public void get_ThrowsOnNoSuchProperty() throws IOException, ParseException {
+        PropertiesParser parser = new PropertiesParser(new FileInputStream("resources/lab5/prop.properties"));
+        parser.parse();
+        assertThrows(IllegalArgumentException.class, () -> parser.get("non existent"));
+    }
+
+    @Test
+    public void get_WorksCorrectly() throws IOException, ParseException {
+        PropertiesParser parser = new PropertiesParser(new FileInputStream("resources/lab5/prop.properties"));
+        parser.parse();
+        assertEquals("https://en.wikipedia.org/", parser.get("website"));
+        assertEquals("This is the value that could be looked up with the key \"key with spaces\".", parser.get("key with spaces"));
+        assertEquals("\u0009", parser.get("tab"));
+    }
 }
