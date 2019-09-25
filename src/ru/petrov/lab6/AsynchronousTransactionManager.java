bdc2998d524e1488e2f7d16c1f3ac0f10d1b212d
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-25 11:05:39 +0300

    Creating class AsynchronousTransactionManager and writing tests for it

---
+++ src/ru/petrov/lab6/AsynchronousTransactionManager.java
@@ -0,0 +1,15 @@
+package ru.petrov.lab6;
+
+import java.util.List;
+
+public class AsynchronousTransactionManager implements TransactionManager {
+
+    public AsynchronousTransactionManager(List<Transaction> transactions) {
+
+    }
+
+    @Override
+    public void completeTransactions() {
+
+    }
+}
---
+++ test/ru/petrov/lab6/AsynchronousTransactionManagerTest.java
@@ -0,0 +1,68 @@
+package ru.petrov.lab6;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AsynchronousTransactionManagerTest {
+
+    private static List<Transaction> transactions;
+    private static Map<Integer, Account> accounts;
+
+    @BeforeAll
+    public static void loadTransactions() throws ParserConfigurationException, IOException, SAXException {
+
+        transactions = new ArrayList<>();
+        accounts = Map.of(
+            0, new Account(0, 200),
+            1, new Account(1, 150),
+            2, new Account(2, 250)
+        );
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        File file = new File("resources/lab6/littleData.xml");
+        Document doc = builder.parse(file);
+        NodeList nodes = doc.getElementsByTagName("transaction");
+        for (int i = 0; i < 10; i++) {
+            Element el = (Element) nodes.item(i);
+            int id = Integer.valueOf(el.getAttribute("id"));
+            int from = Integer.valueOf(el.getAttribute("from"));
+            int to = Integer.valueOf(el.getAttribute("to"));
+            int amount = Integer.valueOf(el.getAttribute("amount"));
+            transactions.add(
+                new Transaction(
+                    id,
+                    accounts.get(from),
+                    accounts.get(to),
+                    amount
+                )
+            );
+        }
+    }
+
+    @Test
+    public void completeTransactions_WorksCorrectly() {
+
+        AsynchronousTransactionManager stm = new AsynchronousTransactionManager(transactions);
+        stm.completeTransactions();
+
+        assertEquals(130, accounts.get(0).getAmount());
+        assertEquals(222, accounts.get(1).getAmount());
+        assertEquals(248, accounts.get(2).getAmount());
+    }
+}
