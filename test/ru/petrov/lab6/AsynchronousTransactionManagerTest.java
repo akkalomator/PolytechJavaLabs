59afac252e542a50c95410fdb84e67177a37ed42
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-09-25 12:35:49 +0300

    Extracting logic of parsing transaction file to class XMLTransactionLoader

--- test/ru/petrov/lab6/AsynchronousTransactionManagerTest.java
+++ test/ru/petrov/lab6/AsynchronousTransactionManagerTest.java
@@ -2,21 +2,15 @@ package ru.petrov.lab6;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AsynchronousTransactionManagerTest {
 
@@ -25,34 +19,14 @@ class AsynchronousTransactionManagerTest {
 
     @BeforeAll
     public static void loadTransactions() throws ParserConfigurationException, IOException, SAXException {
-
-        transactions = new ArrayList<>();
         accounts = Map.of(
             0, new Account(0, 200),
             1, new Account(1, 150),
             2, new Account(2, 250)
         );
-
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        File file = new File("resources/lab6/littleData.xml");
-        Document doc = builder.parse(file);
-        NodeList nodes = doc.getElementsByTagName("transaction");
-        for (int i = 0; i < 10; i++) {
-            Element el = (Element) nodes.item(i);
-            int id = Integer.valueOf(el.getAttribute("id"));
-            int from = Integer.valueOf(el.getAttribute("from"));
-            int to = Integer.valueOf(el.getAttribute("to"));
-            int amount = Integer.valueOf(el.getAttribute("amount"));
-            transactions.add(
-                new Transaction(
-                    id,
-                    accounts.get(from),
-                    accounts.get(to),
-                    amount
-                )
-            );
-        }
+        XMLTransactionsLoader loader = new XMLTransactionsLoader();
+        File input = new File("resources/lab6/littleData.xml");
+        transactions = loader.load(input, accounts);
     }
 
     @Test
--- test/ru/petrov/lab6/SynchronousTransactionManagerTest.java
+++ test/ru/petrov/lab6/SynchronousTransactionManagerTest.java
@@ -25,34 +25,14 @@ class SynchronousTransactionManagerTest {
 
     @BeforeAll
     public static void loadTransactions() throws ParserConfigurationException, IOException, SAXException {
-
-        transactions = new ArrayList<>();
         accounts = Map.of(
             0, new Account(0, 200),
             1, new Account(1, 150),
             2, new Account(2, 250)
         );
-
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        File file = new File("resources/lab6/littleData.xml");
-        Document doc = builder.parse(file);
-        NodeList nodes = doc.getElementsByTagName("transaction");
-        for (int i = 0; i < 10; i++) {
-            Element el = (Element) nodes.item(i);
-            int id = Integer.valueOf(el.getAttribute("id"));
-            int from = Integer.valueOf(el.getAttribute("from"));
-            int to = Integer.valueOf(el.getAttribute("to"));
-            int amount = Integer.valueOf(el.getAttribute("amount"));
-            transactions.add(
-                new Transaction(
-                    id,
-                    accounts.get(from),
-                    accounts.get(to),
-                    amount
-                )
-            );
-        }
+        XMLTransactionsLoader loader = new XMLTransactionsLoader();
+        File input = new File("resources/lab6/littleData.xml");
+        transactions = loader.load(input, accounts);
     }
 
     @Test
